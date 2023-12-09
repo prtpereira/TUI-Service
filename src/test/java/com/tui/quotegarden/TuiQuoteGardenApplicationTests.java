4e444da9a00fa9b7f53e5a59f61c12de38741c1f
André Pereira <andre.pereira@tomiaglobal.com>
2023-12-09 22:30:20 +0000

    Initial spring boot boilerplate

---
+++ src/test/java/com/tui/quotegarden/TuiQuoteGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tui.quotegarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuiQuoteGardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
